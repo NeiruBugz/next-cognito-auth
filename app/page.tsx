ce29fa6c7afe2fb99111f8c9c42bb60acc2400a4
Nail Badiullin <nbadiullin@provectus.com>
2023-04-20 12:53:40 +0400

    add link button

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,17 @@
 import Link from "next/link";
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-
-        <Link href="/auth/signup" className="mr-2">Sign Up</Link>
+    <main className="min-h-screen items-center justify-between p-24">
+      <Button variant="link">
+        <Link href="/auth/signup" className="mr-2">
+          Sign Up
+        </Link>
+      </Button>
+      <Button variant="link">
         <Link href="/auth/login">Login</Link>
-      </div>
+      </Button>
     </main>
   );
 }
